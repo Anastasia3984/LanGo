37d9346d1e3f31878615503944e1f65953313ab0
Anastasiia <gaboda0981@gmail.com>
2025-10-21 01:11:21 +0300

    linting

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('jest').Config} */
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleFileExtensions: ['js', 'jsx'],
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleFileExtensions: ["js", "jsx"],
 };
